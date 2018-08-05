edbd32210178fd921b8f59556f92d89ef4409d1d
sebbah <sebbah@tlen.pl>
2018-08-05 12:03:15 +0200

    First commit

---
+++ WysSMS/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WysSMS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(77, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Wyslij SMS";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(222, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(182, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(62, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 100);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "SMS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ WysSMS/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WysSMS
+{
+    public partial class Form1 : Form
+    {
+        System.IO.Ports.SerialPort port;
+        string numer = "503......";
+        string wiadomosc = "Testowa wiadomosc ...";
+        string sPortName = "COM5";
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = numer;
+            textBox2.Text = wiadomosc;
+            textBox3.Text = sPortName;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            numer = textBox1.Text;
+            wiadomosc = textBox2.Text;
+            Wyslij(numer, wiadomosc);
+            MessageBox.Show("SMS wysłany ...");
+        }
+
+        private void Pauza() { System.Threading.Thread.Sleep(1000); }
+
+        private void Wyslij(string numer, string wiadomosc)
+        {
+            //inicjalizacja zmiennej port z domyślnymi wartościami
+            port = new SerialPort();
+            //ustawienie timeoutów aby program się nie wieszał
+            port.ReadTimeout = 500;
+            port.WriteTimeout = 500;
+
+            sPortName = textBox3.Text;
+            string sBaud = "9600";
+            string sData = "8";
+            string sParity = "None";
+            string sStop = "One";
+
+            port.PortName = sPortName;
+            port.BaudRate = Int32.Parse(sBaud);
+            port.DataBits = Int32.Parse(sData);
+            port.Parity = (Parity)Enum.Parse(typeof(Parity), sParity);
+            port.StopBits = (StopBits)Enum.Parse(typeof(StopBits), sStop);
+
+            port.Open();
+            Pauza();
+            port.Write("at" + (char)13); // TODO: przy pierwszej komendzie ATodem zwraca "ERROR" zamiast "OK" ?
+            Pauza();
+            port.Write("at+cmgf=1" + (char)13);
+            Pauza();
+            port.Write("at+cmgs=" + (char)34 + numer + (char)34 + (char)13);
+            Pauza();
+            port.WriteLine(wiadomosc + (char)26 + (char)13);
+            Pauza();
+            port.Close();
+        }
+    }
+}
